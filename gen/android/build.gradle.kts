1be36fd29144c0b40ea8d06f0aeaaaf260ce2e55
Jeremy Metz <metz.jp@gmail.com>
2024-10-09 14:00:20 +0200

    Added gen/android folder except for gen/android/app

---
+++ gen/android/build.gradle.kts
@@ -0,0 +1,21 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.0.0")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register("clean").configure {
+    delete("build")
+}
+
